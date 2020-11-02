71135e7f5021fc964d41dcff6d97716a11676074
Anindya Kundu <anindya.k22@outlook.com>
2020-11-02 13:07:47 +0530

    Add history page contents

--- frontend/src/components/page-components/HistoryContent.tsx
+++ frontend/src/components/page-components/HistoryContent.tsx
@@ -1,11 +1,97 @@
-import React from 'react';
+import React, { useContext, useEffect, useState } from 'react';
+
+import AuthContext from '../../context/auth-context';
 
 import './HistoryContent.scss';
 
 export default function HistoryContent() {
+  const authContext = useContext(AuthContext);
+  const [userHistory, setUserHistory] = useState([]);
+
+  useEffect(() => {
+    const nameRequestBody = {
+      query: `
+        query transactions($userID: String!) {
+          transactions(userID: $userID) {
+            transID
+            bookID
+            borrowDate
+            returnDate
+          }
+        }
+      `,
+      variables: {
+        userID: authContext.userID || '11118001'
+      }
+    };
+
+    fetch('http://localhost:8000/api', {
+      method: 'POST',
+      body: JSON.stringify(nameRequestBody),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    })
+      .then((response) => {
+        if (response.status !== 200 && response.status !== 201) {
+          throw new Error('failed!');
+        }
+        return response.json();
+      })
+      .then((responseData) => {
+        if (responseData.data.transactions) {
+          setUserHistory(responseData.data.transactions);
+        }
+      })
+      .catch((e) => {
+        console.error(e);
+      });
+  }, []);
+
+  const parseDate = (isodate: string) => {
+    let date = new Date(isodate);
+    return date.toUTCString();
+  };
+
   return (
     <div id="history-content" className="container">
-      <h1>Hello</h1>
+      <h2>History of transactions</h2>
+      <div id="history-table">
+        <table>
+          <thead>
+            <tr>
+              <th>ID</th>
+              <th>Book ID</th>
+              <th>Borrow Date</th>
+              <th>Return Date</th>
+              <th>Status</th>
+            </tr>
+          </thead>
+          <tbody>
+            {userHistory.map((historyItem: any, index) => (
+              <tr key={`history-item-${index}`}>
+                <td>{historyItem.transID}</td>
+                <td>{historyItem.bookID}</td>
+                <td>{parseDate(historyItem.borrowDate)}</td>
+                <td>
+                  {historyItem.returnDate === ''
+                    ? '-'
+                    : parseDate(historyItem.returnDate)}
+                </td>
+                <td
+                  style={
+                    historyItem.returnDate === ''
+                      ? { color: 'red' }
+                      : { color: 'green' }
+                  }
+                >
+                  {historyItem.returnDate === '' ? 'pending' : 'returned'}
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 }
