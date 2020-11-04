b4297276001e60b02fce0490603aaf1655dac51f
Anindya Kundu <anindya.k22@outlook.com>
2020-11-04 23:49:31 +0530

    More transaction functions

--- backend/src/graphql/fields/transaction.ts
+++ backend/src/graphql/fields/transaction.ts
@@ -2,10 +2,17 @@ import {
     GraphQLFieldConfigMap,
     GraphQLList,
     GraphQLNonNull,
+    GraphQLScalarType,
     GraphQLString
 } from 'graphql';
 import { GQLTransaction } from '../types/transaction';
-import { borrowBook, returnBook, transactions } from '../resolvers/transaction';
+import {
+    borrowBook,
+    outstanding,
+    pending,
+    returnBook,
+    transactions
+} from '../resolvers/transaction';
 
 export const TransactionQueries: GraphQLFieldConfigMap<any, any> = {
     transactions: {
@@ -15,6 +22,22 @@ export const TransactionQueries: GraphQLFieldConfigMap<any, any> = {
             userID: { type: GraphQLNonNull(GraphQLString) }
         },
         resolve: (_, args) => transactions(args.userID)
+    },
+    pending: {
+        type: GraphQLList(GQLTransaction),
+        description: 'A list of pending transactions',
+        args: {
+            userID: { type: GraphQLNonNull(GraphQLString) }
+        },
+        resolve: (_, args) => pending(args.userID)
+    },
+    outstanding: {
+        type: GraphQLList(GQLTransaction),
+        description: 'A list of outstanding transactions',
+        args: {
+            userID: { type: GraphQLNonNull(GraphQLString) }
+        },
+        resolve: (_, args) => outstanding(args.userID)
     }
 };
 
--- backend/src/graphql/resolvers/transaction.ts
+++ backend/src/graphql/resolvers/transaction.ts
@@ -18,6 +18,27 @@ export async function borrowBook(
         throw new Error('Invalid user or book');
     }
 
+    const userOpenTrans = await Transaction.find({ userID });
+    let counter = 0;
+    userOpenTrans.forEach((trans) => {
+        if (trans.returnDate === null || trans.returnDate === '') {
+            counter++;
+        }
+    });
+    if (counter === 5) {
+        throw new Error('Quota full');
+    }
+    userOpenTrans.forEach((trans) => {
+        if (trans.bookID === bookID) {
+            throw new Error('Book already borrowed');
+        }
+    });
+
+    const bookCount = (await Book.findOne({ bookID }))?.quantity;
+    if (!bookCount || bookCount === 0) {
+        throw new Error('No more in stock');
+    }
+
     const transaction: ITransactionDoc = new Transaction({
         transID,
         userID,
@@ -28,6 +49,9 @@ export async function borrowBook(
         returnDate: null
     });
     const doc: ITransactionDoc = await transaction.save();
+
+    await Book.updateOne({ bookID }, { $set: { quantity: bookCount - 1 } });
+
     return { ...doc._doc };
 }
 
@@ -44,6 +68,14 @@ export async function returnBook(
         { returnDate: new Date().toISOString() }
     );
     const updatedTransaction = await Transaction.findOne({ transID });
+
+    const bookCount = (await Book.findOne({ bookID: transaction.bookID }))
+        ?.quantity;
+    await Book.updateOne(
+        { bookID: transaction.bookID },
+        { $set: { quantity: bookCount + 1 } }
+    );
+
     return !updatedTransaction ? null : updatedTransaction._doc;
 }
 
@@ -51,4 +83,34 @@ export async function transactions(userID: string): Promise<ITransaction[]> {
     const user = await User.findOne({ userID });
     if (!user) throw new Error('No User found');
     return await Transaction.find({ userKey: user._id });
+}
+
+export async function pending(userID: string): Promise<ITransaction[]> {
+    const user = await User.findOne({ userID });
+    if (!user) throw new Error('No User found');
+    const transAll = await Transaction.find({ userKey: user._id });
+    let transPending: ITransaction[] = [];
+    transAll.forEach((trans) => {
+        if (trans.returnDate === '' || trans.returnDate === null) {
+            transPending.push(trans);
+        }
+    });
+    return transPending;
+}
+
+export async function outstanding(userID: string): Promise<ITransaction[]> {
+    const user = await User.findOne({ userID });
+    if (!user) throw new Error('No User found');
+    const transAll = await Transaction.find({ userKey: user._id });
+    let transPending: ITransaction[] = [];
+    transAll.forEach((trans) => {
+        if (trans.returnDate === '' || trans.returnDate === null) {
+            let bdate = new Date(trans.borrowDate).getMilliseconds();
+            let rdate = new Date().getMilliseconds();
+            let diff = Math.round((rdate - bdate) / (1000 * 60 * 60 * 24));
+            console.log(diff);
+            transPending.push(trans);
+        }
+    });
+    return transPending;
 }
