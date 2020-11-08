a9565ef19dfb96f5f22ad0be42f53f5d85fbf721
Anindya Kundu <anindya.k22@outlook.com>
2020-11-09 02:00:49 +0530

    Add footer

---
+++ frontend/src/components/Footer.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+import './Footer.scss';
+
+export default function Footer() {
+  return (
+    <footer className="container-fluid">
+      <div className="container">
+        <h1>mylibrary.inc</h1>
+        <h2>
+          Connects – people to people, people to place, people to learning.
+          <br />
+          Cutting libraries in a recession is like cutting hospitals in a
+          plague.
+        </h2>
+        <h3>Copyright &copy;2020 meganindya. All rights reserved.</h3>
+      </div>
+    </footer>
+  );
+}
--- frontend/src/pages/Template.tsx
+++ frontend/src/pages/Template.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 
 import SideBar from '../components/SideBar';
 import NavBar from '../components/NavBar';
+import Footer from '../components/Footer';
 import BrowseContent from '../components/page-components/BrowseContent';
 import HistoryContent from '../components/page-components/HistoryContent';
 import DashboardContent from '../components/page-components/DashboardContent';
@@ -19,10 +20,17 @@ export default function TemplatePage(props: ITemplatePage) {
         <SideBar activeLinkName={props.pageName} />
       </div>
       <div id="page-body">
-        <NavBar />
-        {props.pageName === 'browse' && <BrowseContent />}
-        {props.pageName === 'history' && <HistoryContent />}
-        {props.pageName === 'dashboard' && <DashboardContent />}
+        <div id="navbar-wrap">
+          <NavBar />
+        </div>
+        <div id="template-content">
+          {props.pageName === 'browse' && <BrowseContent />}
+          {props.pageName === 'history' && <HistoryContent />}
+          {props.pageName === 'dashboard' && <DashboardContent />}
+        </div>
+        <div id="footer-wrap">
+          <Footer />
+        </div>
       </div>
     </React.Fragment>
   );
