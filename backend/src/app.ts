2f2be8b6e423eb6a505f7c31b52f86b443e43075
Anindya Kundu <anindya.k22@outlook.com>
2020-11-15 10:39:04 +0530

    Configure postgreSQL server

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,11 +1,19 @@
 import express, { Application } from 'express';
+import dotenv, { DotenvConfigOutput } from 'dotenv';
 import { graphqlHTTP } from 'express-graphql';
 import schema from './graphql/schema';
 import mongoose from 'mongoose';
+import { Client } from 'pg';
 
 // load ENVIRONMENT VARIABLES
-import dotenv from 'dotenv';
-dotenv.config();
+try {
+    const configOutput: DotenvConfigOutput = dotenv.config();
+    if (configOutput['error'] !== undefined || configOutput['parsed'] === undefined)
+        throw new Error("cannot find databases' configuration file '.env'");
+    console.log("databases' configuration file loaded");
+} catch (err) {
+    console.error(err);
+}
 
 // spawn express server
 const app: Application = express();
@@ -25,15 +33,34 @@ app.use((req, res, next) => {
 // handle /api path as graphQL endpoint
 app.use('/api', graphqlHTTP({ schema: schema, graphiql: true }));
 
-// connect to mongoDB Atlas database using mongoose
-mongoose
-    .connect(
-        `mongodb://${process.env.MONGO_USER}:${process.env.MONGO_PASS}@cluster0-shard-00-00.snhzr.mongodb.net:27017,cluster0-shard-00-01.snhzr.mongodb.net:27017,cluster0-shard-00-02.snhzr.mongodb.net:27017/${process.env.MONGO_DB}?ssl=true&replicaSet=atlas-7vq935-shard-0&authSource=admin&retryWrites=true&w=majority`,
+// connection for mongoDB Atlas database using mongoose
+const mongoConnection = async () => {
+    await mongoose.connect(
+        `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASS}@${process.env.MONGO_HOST}/${process.env.MONGO_DB}?retryWrites=true&w=majority`,
         { useNewUrlParser: true, useUnifiedTopology: true }
-    )
-    .then(() =>
+    );
+    console.log('connected to mongoDB server');
+};
+
+// connection for postgreSQL database
+const postgresClient = new Client({
+    connectionString: `postgres://${process.env.POSTGRES_USER}:${process.env.POSTGRES_PASS}@${process.env.POSTGRES_HOST}:${process.env.POSTGRES_PORT}/${process.env.POSTGRES_DB}`
+});
+const postgresConnection = async () => {
+    await postgresClient.connect();
+    console.log('connected to postgreSQL server');
+};
+
+// listen (server) for connections if databases are successfully connected
+(async () => {
+    try {
+        await Promise.all([mongoConnection(), postgresConnection()]);
         app.listen(process.env.PORT || 8000, () =>
             console.log(`listening on port ${process.env.PORT || 8000}`)
-        )
-    )
-    .catch((err) => console.error(err));
+        );
+    } catch (err) {
+        console.error(err);
+    }
+})();
+
+export default postgresClient;
